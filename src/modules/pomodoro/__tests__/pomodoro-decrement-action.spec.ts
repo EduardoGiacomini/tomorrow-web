a0b48bf51ba25ccd8991e7c12bbcd2d989af43c5
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2024-02-16 07:26:23 -0400

    test(pomodoro): unit testing for decrement pomodoro

---
+++ src/modules/pomodoro/__tests__/pomodoro-decrement-action.spec.ts
@@ -0,0 +1,61 @@
+import {
+  POMODORO_INITIAL_STATE,
+  pomodoroReducer,
+  decrementPomodoro,
+} from "../reducers";
+import { PomodoroState } from "../reducers/pomodoro-state";
+
+describe("decrementPomodoro()", () => {
+  describe("given a new pomodoro", () => {
+    describe("when I decrement the pomodoro", () => {
+      test("it should do nothing", () => {
+        const action = decrementPomodoro();
+        const state = pomodoroReducer(POMODORO_INITIAL_STATE, action);
+
+        expect(state.seconds).toBe(0);
+        expect(state.running).toBeFalsy();
+        expect(state.error).toBeUndefined();
+      });
+    });
+  });
+
+  describe("given a running pomodoro with 0 seconds", () => {
+    describe("when I decrement the pomodoro", () => {
+      test("it should do nothing", () => {
+        const action = decrementPomodoro();
+        /// Pomodoro has just finished
+        const runningPomodoroState: PomodoroState = {
+          error: undefined,
+          running: true,
+          seconds: 0,
+        };
+        const state = pomodoroReducer(runningPomodoroState, action);
+
+        expect(state.seconds).toBe(0);
+        /// decrement action is not expected to stop the countdown, it is pause
+        /// action role.
+        expect(state.running).toBeTruthy();
+        expect(state.error).toBeUndefined();
+      });
+    });
+  });
+
+  describe("given a running pomodoro with 30 seconds", () => {
+    describe("when I decrement the pomodoro", () => {
+      test("it should decrement one second", () => {
+        const action = decrementPomodoro();
+        /// Pomodoro has just finished
+        const runningPomodoroState: PomodoroState = {
+          error: undefined,
+          running: true,
+          seconds: 30,
+        };
+        const state = pomodoroReducer(runningPomodoroState, action);
+
+        expect(state.seconds).toBe(29);
+        expect(state.running).toBeTruthy();
+        expect(state.error).toBeUndefined();
+      });
+    });
+  });
+});
